a0aa36ddffdf778bcfcd5b8f42e14d38b66e353f
Manish Kumar Patel <maneeshkurmii@gmail.com>
2025-02-24 16:28:38 +0530

    Adding Screenshots as demo images

--- FraudFilter - Minor Project/frontend/src/components/pages/Contact.jsx
+++ FraudFilter - Minor Project/frontend/src/components/pages/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
         <>
             <section className="text-gray-600 bg-[#142F32] m-3 body-font relative">
                 <div className="container px-5 py-7 mx-auto flex sm:flex-nowrap flex-wrap">
-                    <div className="lg:w-2/3 md:w-1/2 bg-red-800 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    <div className="lg:w-2/3 md:w-1/2 bg-gray-200 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
                         <iframe width="100%" height="100%" className="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?q=vadodara&t=&z=13&ie=UTF8&iwloc=&output=embed" ></iframe>
                         <div className="bg-[#142F32] relative flex flex-wrap py-6 rounded shadow-md">
                             <div className="lg:w-1/2 px-6">
